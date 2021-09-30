0ff9bd7ba2268508aaab56ba24c15337212894f3
JohannaSoi <johanna.k.soini@gmail.com>
2021-09-30 14:44:28 +0300

    final touches

--- Excersice_3/src/components/SearchResult.js
+++ Excersice_3/src/components/SearchResult.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import styles from './SearchResult.module.css';
 
-export default function SearchResult(props) {
+export default function result(props) {
   return (
     <div className={ styles.product }>
         <div>
-          <div><img src={`/images/${props.image}`} /></div>
+          <div> 
+            <img src={`/images/${props.image}`} 
+            height={300}
+            width={300}
+          />
+          </div>
           <div className={ styles.name }>{ props.name }</div>
           <div className={ styles.rating }>{ props.rating }</div>
-          
 <div>
 <span className={ styles.price}>${ props.price }</span> 
 <span className={ styles.prices}>{ props.price2 }</span>
 </div>
-<div className={ styles.info }>{ props.info }</div>
-      
+<div className={ styles.info }>{ props.info }</div>      
         </div>
     </div>
   )
--- Excersice_3/src/components/SearchView.js
+++ Excersice_3/src/components/SearchView.js
@@ -3,14 +3,12 @@ import styles from './SearchView.module.css';
 import SearchResult from './SearchResult';
 export default function SearchView(props) {
 
-
   return (
     <div>
-          <div className={ styles.presentationModeGrid }>
+      <div className={ styles.presentationModeGrid }>
       {
         props.items.map(item => <SearchResult key={item.id} {...item} />)
       }
-
       </div>
     </div>
   )
